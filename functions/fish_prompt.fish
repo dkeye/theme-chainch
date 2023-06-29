187c9da8fcf4b09d4f43cf71dad1d70848f44796
dk <ldifmo@gmail.com>
2023-06-29 16:36:07 +0400

    Update fish_prompt.fish

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,5 @@
 function fish_prompt
   # Save the status code from the previous command.
-  set -l __chain_last_status $status
   set -l IFS ''
 
   set -q chain_prompt_glyph
@@ -19,14 +18,6 @@ function fish_prompt
     printf '┌%s\n└' (__chain_compiled_prompt)
   else
     printf '%s-' (__chain_compiled_prompt)
-  end
-
-  # Prompt arrow with status of last command.
-  if test $__chain_last_status = 0
-    builtin set_color green 2> /dev/null
-  else
-    builtin set_color $fish_color_error 2> /dev/null
-    echo -n "$__chain_last_status-"
   end
 
   echo -n "$chain_prompt_glyph "
